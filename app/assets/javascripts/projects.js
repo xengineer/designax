66f59df958054cc865ca032b8f669491769112d2
hideaki nemoto <hideaki.nemoto@nubee.com>
2014-03-20 16:41:15 +0900

    2回クリックしないと編集モードに入れない問題改修。

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,40 @@
+
+$(function () {
+  $.fn.editable.defaults.mode = 'inline';
+  $('.project_name').editable();
+
+  $('.project_name').click(function() {
+    $.fn.editable.defaults.ajaxOptions = {type: "PUT"};
+    var dtype  = $(this).attr("data-type")
+    var dpk    = $(this).attr("data-pk")
+    //var tmpurl = $(this).attr("data-url")
+    //alert(tmpurl)
+    var durl = "/projects/" + dpk
+    
+    //alert(durl)
+
+    $(this).editable({
+      type: dtype,
+      pk: dpk,
+      url: durl
+    });
+  });
+
+  $('.new_project').click(function() {
+    $.fn.editable.defaults.ajaxOptions = {type: "POST"};
+    var dtype  = $(this).attr("data-type")
+    //var tmpurl = $(this).attr("data-url")
+    //alert(tmpurl)
+    var durl = "/projects"
+    
+    //alert(durl)
+
+    $(this).editable({
+      type: dtype,
+      pk: "new",
+      url: durl
+    });
+  });
+
+});
+
