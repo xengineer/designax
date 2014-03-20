e1b5a33545422e422569a180e3636f77e2c12c56
hideaki nemoto <hideaki.nemoto@nubee.com>
2014-03-20 16:16:15 +0900

    プロジェクト編集画面のbootstrap対応。(たぶん動かない)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,9 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
 
+    # index.html.haml表示用URL
+    @urlroot = Designax::Application.config.urlroot
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @projects }
@@ -42,7 +45,13 @@ class ProjectsController < ApplicationController
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.new(params[:project])
+    if params[:pk] == "new" and params[:name] == "project_name"
+      project_name = params[:value]
+      @project = Project.new()
+      @project.project_name = project_name
+    else
+      @project = Project.new(params[:project])
+    end
 
     respond_to do |format|
       if @project.save
@@ -58,13 +67,22 @@ class ProjectsController < ApplicationController
   # PUT /projects/1
   # PUT /projects/1.json
   def update
+    logger.debug "################################ update ##################################\n"
     @project = Project.find(params[:id])
+    logger.debug "################################" + @project.project_name.to_s + "##################################\n"
+
+    if params[:name] == "project_name"
+      project_name = params[:value]
+      @project.project_name = project_name
+    end
 
     respond_to do |format|
       if @project.update_attributes(params[:project])
+        logger.debug "################################ success ##################################\n"
         format.html { redirect_to @project, notice: 'Project was successfully updated.' }
         format.json { head :no_content }
       else
+        logger.debug "################################ fail ##################################\n"
         format.html { render action: "edit" }
         format.json { render json: @project.errors, status: :unprocessable_entity }
       end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@
 
 class Project < ActiveRecord::Base
   attr_accessible :project_name
+
+  validates :project_name,
+            :presence => {:message => " no project_name."},
+            :uniqueness => true
 end
