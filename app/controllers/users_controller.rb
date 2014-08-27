b35ffb89c23abddd2511cf97d3634c05828bf141
hideaki nemoto <hideaki.nemoto@nubee.com>
2014-08-27 12:02:42 +0900

    added function to
    - disable user
    - edit all user information(admin only)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,59 @@
+# coding: utf-8
+
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :authorize, :only => 'index'
+  before_filter :authorize, :only => ['index', 'edit', 'update', 'destroy']
 
   def index
-    @users = User.all
-    print @users.to_s
+    @users_active   = User.where(["delflag = ?", '0'])
+    @users_disabled = User.where(["delflag = ?", '1'])
+
     respond_to do |format|
       format.html
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    posted_user = params[:user]
+    new_password = posted_user[:new_password]
+    password_confirmation = posted_user[:password_confirmation]
+    role = posted_user[:role]
+
+    respond_to do |format|
+      if new_password != password_confirmation
+        format.html { redirect_to users_path, :alert => 'confirmation password が一致しません.' }
+      end
+
+      if @user.update_attributes(:password => new_password, :role => role)
+        format.html { redirect_to users_path, notice: 'Password was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "index" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(:delflag => 'true')
+        format.html { redirect_to users_path, notice: "Account #{@user.username} was successfully disabled." }
+        format.json { head :no_content }
+      else
+        format.html { render action: "index" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
+# coding: utf-8
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
-  attr_accessible :role, :login, :user_group_id
+  attr_accessible :role, :login, :user_group_id, :delflag
   # attr_accessible :title, :body
   attr_accessor :login
   belongs_to :user_group
@@ -16,6 +18,25 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :username, :message => "this username is taken"
   validate :username_is_present
+
+  def active_for_authentication?
+    super && self.account_active?
+  end
+
+  def account_active?
+    if self.delflag.to_s == 'true'
+      logMessage = self.class.to_s + "#" + __method__.to_s + " " + :username.to_s +
+                   " DELFLAG: " + self.delflag.to_s
+
+      logger.info logMessage
+      return false
+    end
+    return true
+  end
+
+  def inactive_message
+    "メールアドレスまたはパスワードが間違っています。"
+  end
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
